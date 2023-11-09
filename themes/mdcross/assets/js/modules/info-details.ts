d7f86237a4a01eb46d773a564e626644369c7291
Nikolay Kolomyytsev <kolya.ya@gmail.com>
2023-11-09 01:04:25 +0100

    feat: added animation for details/summary shortcode

--- themes/mdcross/assets/js/modules/info-details.ts
+++ themes/mdcross/assets/js/modules/info-details.ts
@@ -1,9 +1,20 @@
-console.log('InfoDet');
+console.log('Info Details');
 
-const infoDetails = document.querySelectorAll('.info__details')
+const infoDetails: NodeListOf<HTMLElement> = document.querySelectorAll('.info__details33')
 
-infoDetails.forEach(iD => {
+infoDetails.forEach((iD: HTMLElement) => {
     iD.addEventListener('click', (event) => {
-        infoDetails.forEach(iD => iD.removeAttribute('open'))
+//         infoDetails.forEach(iD => iD.removeAttribute('open'))
+        if (iD.hasAttribute('open')) {
+            event.preventDefault()
+            iD.classList.add('closing')
+
+            iD.addEventListener('animationend', (event: AnimationEvent) => {
+                if (event.animationName === 'close') {
+                    iD.classList.remove('closing')
+                    iD.removeAttribute('open')
+                }
+            }, { once: true })
+        }
     })
 })
